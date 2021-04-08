78044b517a0e411887242e8b1a53b7f972dfed6e
Noan <noan.crespel@epita.fr>
2021-04-08 19:40:07 +0200

    Dicho

--- Exercices/Exercice6.cs
+++ Exercices/Exercice6.cs
@@ -1,7 +1,55 @@
-﻿namespace Exercices
+﻿using System;
+
+namespace Exercices
 {
     public class Exercice6
     {
+        private int[] arr;
+
+        private void InitArr()
+        {
+            int[] tab = new int[50];
+            Random random = new Random();
+            for (int i = 0; i < 100; i+=2)
+            {
+                tab[i / 2] = i;
+            }
+
+            arr = tab;
+        }
+
+        public bool Dicho(int element)
+        {
+            InitArr();
+            int min = 0;
+            int max = arr.Length-1;
+            int mid;
+            if (element > arr[max] || element < arr[min])
+            {
+                return false;
+            }
+            else
+            {
+                while (min <= max)
+                {
+                    mid = (min + max) / 2;
+                    if (element == arr[mid])
+                    {
+                        return true;
+                    }
+                    else if (arr[mid] < element)
+                    {
+                        min = mid + 1;
+                    }
+                    else
+                    {
+                        max = mid - 1;
+                    }
+                }
+                return false;
+              
+            } 
+        }
         
     }
 }
--- Exercices/Program.cs
+++ Exercices/Program.cs
@@ -1,11 +1,17 @@
+using System;
+
 namespace Exercices
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Exercice5 exercice5 = new Exercice5();
-            exercice5.Game();
+            //Pour tester les exo de 1 a 3, faire ex_.main() avec _=numéro 
+            //Exercice5 exercice5 = new Exercice5();
+            //exercice5.Game();
+            Exercice6 ex6 = new Exercice6();
+            bool res = ex6.Dicho(51);
+            Console.WriteLine(res);
         }
     }
 }
